10a9d3e921c4a6a057c155ba4d471396f62ffd1e
kyBa-Le <lekyba2000hc@gmail.com>
2024-10-10 16:36:03 +0700

    done create row function

--- public/JS/Cart.js
+++ public/JS/Cart.js
@@ -2,31 +2,33 @@
 document.addEventListener('DOMContentLoaded',renderCart(orders));
 import { getQueryParam } from "../../src/Utils/Param.js";
 import { OrderService } from "../../src/Service/OrderService.js";
+import { ProductService } from "../../src/Service/ProductService.js";
 
 const id = getQueryParam("user_id");
 const orders = OrderService.getOrderByUserId(id); // lấy tất cả các đơn hàng trong local
 
 // Hàm tạo ra các hàng cho order
 function createOrderRow(order){
+    var product = ProductService.getProductById(order.user_id);
     return `<tr>
               <td>
                 <img
-                  src="https://images.pexels.com/photos/27420469/pexels-photo-27420469/free-photo-of-dia-kh-e-m-nh-b-a-t-i-b-a-tr-a.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt="${order.name}"
+                  src= ${product.image_link}
+                  alt="${product.name}"
                 />
               </td>
-              <td>${order.name}</td>
-              <td>50.000₫</td>
+              <td>${product.name}</td>
+              <td>${product.price}₫</td>
               <td>
                 <div class="d-flex justify-content-center">
-                  <button class="btn btn-sm btn-outline-secondary">-</button>
+                  <button class="btn btn-sm btn-outline-secondary" onclick=subtractQuantity()>-</button>
                   <input
                     type="text"
-                    value="1"
+                    value=${order.quantity}
                     class="form-control form-control-sm mx-2 text-center"
                     style="width: 50px"
                   />
-                  <button class="btn btn-sm btn-outline-secondary">+</button>
+                  <button class="btn btn-sm btn-outline-secondary" onclick=addQuantity()>+</button>
                 </div>
               </td>
               <td>${e}₫</td>
